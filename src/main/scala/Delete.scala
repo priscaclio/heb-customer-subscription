983be5474093a232e2d4f0446440a3319057642a
MaximeFritel <maxime.fritel@etu.u-pec.fr>
2022-12-03 20:19:26 +0100

    suppresion version parquet à tester

--- src/main/scala/Delete.scala
+++ src/main/scala/Delete.scala
@@ -1,6 +1,7 @@
 package fr.episen.dataprocesing
 
-import org.apache.spark.sql.SparkSession
+import fr.episen.dataprocesing.models.Client
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType, TimestampType}
 
 
@@ -19,5 +20,26 @@ object Delete {
             StructField("Date de Souscription", TimestampType)
           )
         )
+    val list_id_client = args.toList
+    val id_client_string = list_id_client.head
+    val id_client_int = id_client_string.toInt
+
+//    //pour la récupération des csv
+//    val dataFrame: DataFrame = sparkSession.read
+//      .schema(schema)
+//      .option("header", value = true)
+//      .csv("data")
+//    dataFrame.show()
+//    dataFrame.printSchema()
+//
+//    val dataset_filtrer = dataFrame.where("identifiantClient ="+id_client_int)
+
+
+    import sparkSession.implicits._
+    val dataset = sparkSession.read.parquet("data").as[Client]
+    dataset.filter("identifiantClient != " + id_client_int)
+
+
+
   }
 }
