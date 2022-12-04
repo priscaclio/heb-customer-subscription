36f6c8037796c713f084ead85b67faf87ebe2973
prisca.clio <prisca.clio@gmail.com>
2022-12-04 20:47:02 +0100

    scopt option args functional and service delete almost functional

--- src/main/scala/Delete.scala
+++ src/main/scala/Delete.scala
@@ -10,8 +10,9 @@ object Delete {
 
   //Scopt pour définir les arguments pour le service
   case class Config(
-     delete: Boolean = false,
-     hasher: Boolean = false,
+     //delete: Boolean = false,
+     //hasher: Boolean = false,
+     service: String = "",
      id: Long = -1,
      inputpath: String = "",
      outputpath: String = ""
@@ -23,21 +24,35 @@ object Delete {
     OParser.sequence(
       programName("heb-customer-subscription"),
       head("heb-customer-subscription", "1.0"),
-      opt[Boolean]('d', "delete")
+      /*opt[Boolean]('d', "delete")
         //.required()
         .action((d, c) => c.copy(delete = d))
         .text("required boolean"),
       opt[Boolean]('h', "hasher")
         .action((h, c) => c.copy(hasher = h))
-        .text("required boolean"),
+        .text("required boolean"),*/
+      opt[String]('s', "service")
+        .action((h, c) => c.copy(service = h))
+        .required()
+        .validate(h => {
+          if (h == "delete" || h == "hasher") {
+            success
+          } else {
+            failure("wong argument, you have to put 'delete' or 'hasher' with arg --service")
+          }
+        })
+        .text("required string delete or hasher"),
       opt[Long]('u', "id")
         .action((u, c) => c.copy(id = u))
+        .required()
         .text("required long id "),
       opt[String]('i', "inputpath")
         .action((i, c) => c.copy(inputpath = i))
+        .required()
         .text("required string path with csv file"),
       opt[String]('o', "outputpath")
         .action((o, c) => c.copy(outputpath = o))
+        .required()
         .text("required string path to put the new csv file")//,
       /*checkConfig(c => {
         if (c.inputpath < c.outputpath) {
@@ -57,17 +72,66 @@ object Delete {
       case Some(config) =>
         println(OParser.usage(argParser))
       // do stuff with config
-        println("res delete args "+config.delete)
-        println("res delete args "+config.hasher)
-        println("res delete args "+config.id)
-        println("res delete args "+config.inputpath)
-        println("res delete args "+config.outputpath)
+        println("res service args "+config.service)
+        println("res args "+config.id)
+        println("res args "+config.inputpath)
+        println("res args "+config.outputpath)
+
+        //Sparksession
+
+        import org.apache.spark.sql.functions._
+
+        val sparkSession = SparkSession.builder().master("local").getOrCreate()
+
+        val dataframe: DataFrame = sparkSession.read.option("header",true).csv(config.inputpath)
+        //print data
+        dataframe.show()
+        //print schema of csv file
+        dataframe.printSchema()
+
+        //TODO mapper data with json file config
+
+      //Vérif id customer exist and id customer is unique
+        val selectID = dataframe.filter(col("IdentifiantClient")===config.id).count()
+
+        //if ID customer doesn't exist
+        if(selectID == 0){
+          println("The specified ID does not exist")
+        }
+
+        //if ID customer isn't unique
+        if(selectID > 1){
+          println("The provided ID is not unique, there are "+selectID+" lines")
+        }
+
+        //if ID custumer is unique
+        if(selectID==1){
+          //Service to delete a customer in a csv file
+          if(config.service == "delete"){
+            println("Service delete start")
+
+            dataframe.filter(dataframe("IdentifiantClient")!==config.id).show(false)
+            val df = dataframe.filter(dataframe("IdentifiantClient")!==config.id).toDF()
+            df.write.option("header",true).csv(config.outputpath);
+
+            //TODO replace csv files in inputpath by csv files in outputpath
+
+          }
+          //Service to hash customer data in a csv file
+          if(config.service == "hasher"){
+            println("Service hasher start")
+
+            //TODO service
+          }
+        }
+
+
       case _ =>
         //println("you have to put some args")
         //println(OParser.usage(argParser))
 
     }
-
+/*
     import org.apache.spark.sql.functions._
 
     val sparkSession = SparkSession.builder().master("local").getOrCreate()
@@ -102,5 +166,7 @@ object Delete {
     //dataframe.filter(dataframe("IdentifiantClient")!==4).write.mode(SaveMode.Overwrite).csv("data/data1.csv")
     //dataframe.filter(dataframe("IdentifiantClient")!==4).write.format("csv").save("/data/data2.csv")
     //dataframe.show()
+
+ */
   }
 }
