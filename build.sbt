e4b2e805a38721915843d163e2c216d8035c7da5
prisca.clio <prisca.clio@gmail.com>
2022-12-04 17:45:30 +0100

    delete with id customer

--- build.sbt
+++ build.sbt
@@ -10,3 +10,13 @@ libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.8"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.8"
 libraryDependencies += "io.spray" %% "spray-json" % "1.3.5"
 libraryDependencies += "com.github.scopt" %% "scopt" % "4.1.0"
+
+libraryDependencies += "org.zalando" %% "spark-json-schema" % "0.6.1"
+
+dependencyOverrides ++= {
+  Seq(
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.6.7.1",
+    "com.fasterxml.jackson.core" % "jackson-databind" % "2.6.7",
+    "com.fasterxml.jackson.core" % "jackson-core" % "2.6.7"
+  )
+}
--- src/main/scala/Delete.scala
+++ src/main/scala/Delete.scala
@@ -1,7 +1,39 @@
 package fr.episen.dataprocesing
 
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+//import org.zalando.spark.jsonschema.SchemaConverter
+
+import scala.io.Source
+
 object Delete {
   def main(args: Array[String]): Unit = {
+    import org.apache.spark.sql.functions._
 
+    val sparkSession = SparkSession.builder().master("local").getOrCreate()
+
+    //val fileContents = Source.fromFile("data/schemaconfig.json").getLines.mkString
+    //val schema = SchemaConverter.convertContent(fileContents)
+
+    //val schema = SchemaConverter.convert("schemaconfig.json")
+
+    val dataframe: DataFrame = sparkSession.read.option("header",true).csv("C:\\Users\\Prisca\\Documents\\data")
+
+    dataframe.show()
+    dataframe.printSchema()
+
+    //test 1
+    //dataframe.createOrReplaceTempView("tab")
+    //val res = sparkSession.sql("select * from tab where IdentifiantClient=4")
+    //res.show()
+
+    //test 2
+    dataframe.filter(dataframe("IdentifiantClient")!==1).show(false)
+    val df = dataframe.filter(dataframe("IdentifiantClient")!==1).toDF()
+    df.write.csv("C:/Users/Prisca/Documents/data/test2");
+    //dataframe.write.format("csv").save("/tmp/spark_output/datacsv")
+    //dataframe.write.mode(SaveMode.Overwrite).csv("data/data1.csv")
+    //dataframe.filter(dataframe("IdentifiantClient")!==4).write.mode(SaveMode.Overwrite).csv("data/data1.csv")
+    //dataframe.filter(dataframe("IdentifiantClient")!==4).write.format("csv").save("/data/data2.csv")
+    //dataframe.show()
   }
 }
